52641f470f679a486e81f9497713f35fa9365ec7
Christina Bergey <christina.bergey@gmail.com>
2016-09-15 21:41:36 -0400

    Added script to download rhesus genome

---
+++ genomes/download_rheMac2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Script to download macaque genome, rheMac2
+
+mkdir genomes/rheMac2
+cd genomes/rheMac2
+
+GENOME_FA=rheMac2.fa
+
+wget \
+    'ftp://hgdownload.cse.ucsc.edu/goldenPath/rheMac2/bigZips/chromFa.tar.gz' \
+    -O ${GENOME_FA}.tar.gz
+
+tar -zxvf ${GENOME_FA}.tar.gz
+
+cat `ls -v softMask/chr*.fa | grep -v "chrUr"` > $GENOME_FA
