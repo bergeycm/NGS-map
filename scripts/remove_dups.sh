395bc631307f51740beeaab3abb921add5b95b24
Christina Bergey <christina.bergey@gmail.com>
2017-12-07 12:18:11 -0500

    Removed temporary directory from RemoveDups script.

--- scripts/remove_dups.sh
+++ scripts/remove_dups.sh
@@ -7,8 +7,8 @@
 # Check that genome code was passed as parameter
 USAGE="$0 genome_code";
 if [ -z "$1" ]; then
-	echo "ERROR: $USAGE";
-	exit 1;
+    echo "ERROR: $USAGE";
+    exit 1;
 fi
 
 GENOME_CODE=$1
@@ -17,44 +17,39 @@ GENOME_CODE=$1
 
 if [ "$MARK_DUPS" = "TRUE" ]; then
 
-	# Make temp folder
-	TMP_DIR=tmp/$RANDOM
-	mkdir -p $TMP_DIR
+    echo "CMD: java \
+        -jar ${PICARD}/picard.jar MarkDuplicates \
+        INPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam \
+        OUTPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam \
+        M=reports/duplicate_report.txt \
+        VALIDATION_STRINGENCY=SILENT \
+        REMOVE_DUPLICATES=false \
+        MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=${PICARD_MARK_DUP_MAX_FILES}";
 
-echo "CMD: java -Djava.io.tmpdir=${TMP_DIR} \
-		-jar ${PICARD}/picard.jar MarkDuplicates \
-		INPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam \
-		OUTPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam \
-		M=reports/duplicate_report.txt \
-		VALIDATION_STRINGENCY=SILENT \
-		REMOVE_DUPLICATES=false \
-		MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=${PICARD_MARK_DUP_MAX_FILES}";
+    java \
+        -jar ${PICARD}/picard.jar MarkDuplicates \
+        INPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam \
+        OUTPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam \
+        M=reports/duplicate_report.txt \
+        VALIDATION_STRINGENCY=SILENT \
+        REMOVE_DUPLICATES=false \
+        MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=${PICARD_MARK_DUP_MAX_FILES}
 
-	java -Djava.io.tmpdir=${TMP_DIR} \
-		-jar ${PICARD}/picard.jar MarkDuplicates \
-		INPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam \
-		OUTPUT=results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam \
-		M=reports/duplicate_report.txt \
-		VALIDATION_STRINGENCY=SILENT \
-		REMOVE_DUPLICATES=false \
-		MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=${PICARD_MARK_DUP_MAX_FILES}
-
-
-	# Delete temp folder
-	rm -r $TMP_DIR
+    # Delete temp folder
+    rm -r $TMP_DIR
 
 elif [ "$MARK_DUPS" = "FALSE" ]; then
 
-	# Otherwise just copy input file to output
-	echo "Duplicates NOT being removed, as instructed by user."
-	cp results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam
-	cp results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam.bai results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam.bai
-	
+    # Otherwise just copy input file to output
+    echo "Duplicates NOT being removed, as instructed by user."
+    cp results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam
+    cp results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.bam.bai results/${IND_ID_W_PE_SE}.bwa.${GENOME_CODE}.fixed.filtered.postdup.bam.bai
+
 else
 
-	echo "ERROR: MARK_DUPS must be equal to either TRUE or FALSE. Correct error in config.mk." 1>&2 ;
-	exit 1;
-	
+    echo "ERROR: MARK_DUPS must be equal to either TRUE or FALSE. Correct error in config.mk." 1>&2 ;
+    exit 1;
+
 fi
 
 exit;
